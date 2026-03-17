756387c4c9f6258f35bf9ac0ab9c09870f9ac228
Willie Pai <PaisWillie@gmail.com>
2026-03-17 15:38:10 -0400

    fix: refactor shuffle ordering

--- src/main/java/frc/robot/subsystems/intake/LinearIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/LinearIntakeSubsystem.java
@@ -183,12 +183,11 @@ public class LinearIntakeSubsystem extends SubsystemBase {
     public Command shuffle() {
         return Commands.sequence(
                 setPosition(LinearIntakeConstants.SHUFFLE_FAR_POSITION),
-                setPosition(LinearIntakeConstants.SHUFFLE_CLOSE_POSITION)
-                        .andThen(
-                                Commands.sequence(
-                                        retract(),
-                                        fullyRetract())
-                                        .repeatedly()));
+                setPosition(LinearIntakeConstants.SHUFFLE_CLOSE_POSITION)).andThen(
+                        Commands.sequence(
+                                retract(),
+                                fullyRetract())
+                                .repeatedly());
     }
 
     public Command set(double dutycycle) {
